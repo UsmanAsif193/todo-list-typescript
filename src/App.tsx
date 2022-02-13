b07b284c7c2f57b84a0af2daca283a0c74669c0d
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-02-13 19:54:07 +0500

    feat: completed the todoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,55 @@
-import React, { FC } from "react";
+import React, { FC, ChangeEvent, useState } from "react";
+import "./App.css";
+import TodoTask from "./Components/todoTask";
+import { ITask } from "./Interfaces";
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    if (e.target.name === "task") setTask(e.target.value);
+    else setDeadline(Number(e.target.value));
+  };
+
+  const handleAddTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline };
+    setTodoList([...todoList, newTask]);
+    setTask("");
+    setDeadline(0);
+  };
+
+  const completeTask = (taskNameToDelte: string): void => {
+    setTodoList(todoList.filter((task) => task.taskName !== taskNameToDelte));
+  };
+
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder="Task..." />
-          <input type="number" placeholder="Deadline (in Days)..." />
+          <input
+            type="text"
+            placeholder="Task..."
+            name="task"
+            value={task}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Deadline (in Days)..."
+            name="deadline"
+            value={deadline}
+            onChange={handleChange}
+          />
         </div>
-        <button>Add Task</button>
+        <button onClick={handleAddTask}>Add Task</button>
       </div>
-      <div className="todoList"></div>
+      <div className="todoList">
+        {todoList.map((task: ITask, key: number) => (
+          <TodoTask key={key} task={task} completeTask={completeTask} />
+        ))}
+      </div>
     </div>
   );
 };
